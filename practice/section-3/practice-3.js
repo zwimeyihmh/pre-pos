eb88287ee48d6fa609b25b34af179a1baa62be2d
zwimeyihmh <zwimeyihmh@outlook.com>
2015-07-11 23:27:56 +0800

    消除重复函数

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,56 +1,46 @@
-function notInResult(element,result)
-{
-  for(var i = 0; i < result.length; i++){
-    if(element === result[i].key){
+function notInResult(element, result) {
+  for (var i = 0; i < result.length; i++) {
+    if (element === result[i].key) {
       return false;
     }
   }
   return true;
 }
 
-function findSame(element,array){
+function count_same_elements(collection) {
+  var result = [];
+  for (var i = 0; i < collection.length; i++) {
+    if (notInResult(collection[i], result)) {
+      var sum = findSame(collection[i], collection);
+      if (sum > 0) {
+        result.push({
+          key: collection[i],
+          count: sum
+        });
+      }
+    }
+  }
+  return result;
+}
+function findSame(element, array) {
   var count = 0;
-  for(var x = 0; x < array.length; x++){
-    if(element === array[x]){
+  for (var x = 0; x < array.length; x++) {
+    if (element === array[x]) {
       count++;
     }
   }
   return count;
 }
 
-function count_same_elements(collection) {
-  var result = [];
-  for(var i = 0; i < collection.length; i++){
-    if(notInResult(collection[i],result)){
-      var sum = findSame(collection[i],collection);
-      if(sum > 0){
-        result.push({key:collection[i],count:sum});
-      }
-    }
-  }
-  return result;
-}
-
 function create_updated_collection(collection_a, object_b) {
   var result = [];
   var c = count_same_elements(collection_a);
-  for(var i = 0;i < c.length;i++){
-    if(findBSame(c[i].key,object_b.value)){
-      var temp = parseInt(c[i].count/3);
+  for (var i = 0; i < c.length; i++) {
+    if (findSame(c[i].key, object_b.value)) {
+      var temp = parseInt(c[i].count / 3);
       c[i].count -= temp;
     }
     result.push(c[i]);
   }
   return result;
-}
-
-function findBSame(key,objectB)
-{
-  for(var x = 0; x < objectB.length; x++){
-    if(key === objectB[x])
-    {
-      return true;
-    }
-  }
-  return false;
 }
--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,14 +1,50 @@
-function findNum(array)
-{
-  var temp = "";
-  for(var i = 0; i < array.length; i++)
-  {
-    if(array[i] >= 0 && array[i] <= 9)
-    {
-      temp += array[i];
+function create_updated_collection(collection_a, object_b) {
+  var result = [];
+  var c = count_same_elements(collection_a);
+  for(var i = 0;i < c.length;i++){
+    if(findSame(c[i].key,object_b.value)){
+      var temp = parseInt(c[i].count/3);
+      c[i].count -= temp;
+    }
+    result.push(c[i]);
+  }
+  return result;
+}
+
+function count_same_elements(collection) {
+  var result = [];
+  for(var i = 0; i < collection.length; i++){
+    if(notInResult(collection[i][0],result)){
+      var sum = findSame(collection[i][0],collection);
+//      var sum = findBSame(collection[i][0],collection);
+      if(sum > 0){
+        result.push({key:collection[i][0],count:sum});
+      }
     }
   }
-  return parseInt(temp,10);
+  return result;
+}
+/*
+function findBSame(key,objectB)
+{
+  for(var x = 0; x < objectB.length; x++){
+    if(key === objectB[x])
+    {
+      return true;
+    }
+  }
+  return false;
+}
+*/
+
+function notInResult(element,result)
+{
+  for(var i = 0; i < result.length; i++){
+    if(element === result[i].key){
+      return false;
+    }
+  }
+  return true;
 }
 
 function findSame(element,array){
@@ -25,58 +61,16 @@ function findSame(element,array){
   }
   return count;
 }
-function notInResult(element,result)
-{
-  for(var i = 0; i < result.length; i++){
-    if(element === result[i].key){
-      return false;
-    }
-  }
-  return true;
-}
 
-function notInResult(element,result)
+function findNum(array)
 {
-  for(var i = 0; i < result.length; i++){
-    if(element === result[i].key){
-      return false;
-    }
-  }
-  return true;
-}
-
-function count_same_elements(collection) {
-  var result = [];
-  for(var i = 0; i < collection.length; i++){
-    if(notInResult(collection[i][0],result)){
-      var sum = findSame(collection[i][0],collection);
-      if(sum > 0){
-        result.push({key:collection[i][0],count:sum});
-      }
-    }
-  }
-  return result;
-}
-
-function findBSame(elementA,elementB)
-{
-for(var x = 0; x < elementB.length; x++){
-  if(elementA === elementB[x])
+  var temp = "";
+  for(var i = 0; i < array.length; i++)
   {
-    return true;
-  }
-}
-return undefined;
-}
-function create_updated_collection(collection_a, object_b) {
-  var result = [];
-  var c = count_same_elements(collection_a);
-  for(var i = 0;i < c.length;i++){
-    if(findBSame(c[i].key,object_b.value)){
-      var temp = parseInt(c[i].count/3);
-      c[i].count -= temp;
+    if(array[i] >= 0 && array[i] <= 9)
+    {
+      temp += array[i];
     }
-    result.push(c[i]);
   }
-  return result;
+  return parseInt(temp,10);
 }
